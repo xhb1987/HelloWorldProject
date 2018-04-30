39f07fcfae29b6de20c02621e170f2b1cb49c593
xhb1987 <xianghb1987@gmail.com>
2018-04-30 20:59:57 +0800

    update android project
    update android project

--- android/app/src/main/java/com/helloworldproject/MainApplication.java
+++ android/app/src/main/java/com/helloworldproject/MainApplication.java
@@ -1,39 +1,150 @@
 package com.helloworldproject;
 
+import android.app.Notification;
+import android.content.Context;
 import android.support.annotation.Nullable;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
+import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.*;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
+import com.umeng.soexample.invokenative.DplusReactPackage;
+import com.umeng.soexample.invokenative.RNUMConfigure;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
+import com.umeng.message.MsgConstant;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UTrack;
+import com.umeng.message.entity.UMessage;
+import com.umeng.message.UmengNotificationClickHandler;
 
 import java.util.List;
 import java.util.Arrays;
 
+import com.facebook.soloader.SoLoader;
+
 public class MainApplication extends NavigationApplication {
-  @Override
-  public boolean isDebug() {
-    // Make sure you are using BuildConfig from your own application
-    return BuildConfig.DEBUG;
-  }
+    private static final String TAG = MainApplication.class.getName();
+    private Handler handler;
 
-  protected List<ReactPackage> getPackages() {
-    // Add additional packages you require here
-    // No need to add RnnPackage and MainReactPackage
-    return Arrays.<ReactPackage>asList(
-            new VectorIconsPackage()
-    );
-  }
+    @Override
+    public boolean isDebug() {
+        // Make sure you are using BuildConfig from your own applicatio
+        return BuildConfig.DEBUG;
 
-  @Override
-  public List<ReactPackage> createAdditionalReactPackages() {
-    return getPackages();
-  }
+    }
 
-  @Nullable
-  @Override
-  public String getJSMainModuleName() {
-    return "index";
-  }
+    protected List<ReactPackage> getPackages() {
+        // Add additional packages you require here
+        // No need to add RnnPackage and MainReactPackage
+        return Arrays.<ReactPackage>asList(
+                new VectorIconsPackage(),
+                new DplusReactPackage()
+        );
+    }
 
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
+    }
+
+    @Nullable
+    @Override
+    public String getJSMainModuleName() {
+        return "index";
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, false);
+        RNUMConfigure.init(this, "5ae31695f43e4839f1000064", "Umeng", UMConfigure.DEVICE_TYPE_PHONE,
+                "2ff180aff55552a3d58c4802b2b85d9e");
+        initUpush();
+    }
+
+    private void initUpush() {
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+        handler = new Handler(getMainLooper());
+
+        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
+
+        UmengMessageHandler messageHandler = new UmengMessageHandler() {
+            /**
+             * 自定义消息的回调方法
+             */
+            @Override
+            public void dealWithCustomMessage(final Context context, final UMessage msg) {
+
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        // 对自定义消息的处理方式，点击或者忽略
+                        boolean isClickOrDismissed = true;
+                        if (isClickOrDismissed) {
+                            //自定义消息的点击统计
+                            UTrack.getInstance(getApplicationContext()).trackMsgClick(msg);
+                        } else {
+                            //自定义消息的忽略统计
+                            UTrack.getInstance(getApplicationContext()).trackMsgDismissed(msg);
+                        }
+                        Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @Override
+            public Notification getNotification(Context context, UMessage msg) {
+                switch (msg.builder_id) {
+                    case 1:
+                        Notification.Builder builder = new Notification.Builder(context);
+                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(), R.layout.upush_notification);
+                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon1, getLargeIcon(context, msg));
+                        myNotificationView.setImageViewResource(R.id.notification_large_icon2, getSmallIconId(context, msg));
+                        builder.setContent(myNotificationView)
+                                .setSmallIcon(getSmallIconId(context, msg))
+                                .setTicker(msg.ticker)
+                                .setAutoCancel(true);
+
+                        return builder.getNotification();
+                    default:
+                        //默认为0，若填写的builder_id并不存在，也使用默认。
+                        return super.getNotification(context, msg);
+                }
+            }
+        };
+        mPushAgent.setMessageHandler(messageHandler);
+
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
+            @Override
+            public void dealWithCustomAction(Context context, UMessage msg) {
+                Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+            }
+        };
+
+        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+
+        mPushAgent.register(new IUmengRegisterCallback() {
+            @Override
+            public void onSuccess(String deviceToken) {
+                Log.i(TAG, "device token: " + deviceToken);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                Log.i(TAG, "register failed: " + s + " " + s1);
+            }
+        });
+
+    }
 }
